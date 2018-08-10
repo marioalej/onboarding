a0166f2897403e14d1f251313df5fec16ed6705d
mario.rangel <mario.rangel@globant.com>
2018-08-10 14:20:27 -0500

    Re-written base project version in kotlin with the 2 behaviors developed in that project.

--- app/src/main/java/com/globant/mariorangel/onboarding/MainActivity.kt
+++ app/src/main/java/com/globant/mariorangel/onboarding/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.globant.mariorangel.onboarding
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.globant.mariorangel.onboarding.mvp.model.CountModel
+import com.globant.mariorangel.onboarding.mvp.presenter.CountPresenter
+import com.globant.mariorangel.onboarding.mvp.view.CountView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var presenter: CountPresenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        presenter = CountPresenter(CountModel(), CountView(this))
+    }
+
+    override fun onPause() {
+        super.onPause()
+        presenter.unregister()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        presenter.register()
     }
 }
